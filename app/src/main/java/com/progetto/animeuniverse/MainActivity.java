97406c2fe01d98ba4480ac7b3d6087565cdfc04d
Dan1720 <d.gatti19@campus.unimib.it>
2023-03-18 19:50:16 +0100

    Cambio colore sfondo e inserimento bottom navigation bar

--- app/src/main/java/com/progetto/animeuniverse/MainActivity.java
+++ app/src/main/java/com/progetto/animeuniverse/MainActivity.java
@@ -2,7 +2,10 @@ package com.progetto.animeuniverse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primaryColor)));
     }
 }
