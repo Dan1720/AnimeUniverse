85b79252a65c2b9a496df52cfc75f2f8818f8bfa
Dan1720 <d.gatti19@campus.unimib.it>
2023-03-19 11:48:32 +0100

    aggiunte icone nav bar

--- app/src/main/java/com/progetto/animeuniverse/MainActivity.java
+++ app/src/main/java/com/progetto/animeuniverse/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 
 import java.util.Objects;
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primaryColor)));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#CB94A9\">" + getString(R.string.app_name) + "</font>"));
     }
 }
