ec043c8e659d744dd3b21cf22927953f78d0d1bc
Vivi1824 <v.giuliani3@campus.unimib.it>
2023-03-24 12:54:40 +0100

    Inserito video in main activity

--- app/src/main/java/com/progetto/animeuniverse/MainActivity.java
+++ app/src/main/java/com/progetto/animeuniverse/MainActivity.java
@@ -1,26 +1,68 @@
 package com.progetto.animeuniverse;
 
-import androidx.appcompat.app.ActionBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.ColorDrawable;
+
 import android.os.Bundle;
-import android.text.Html;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toolbar;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.widget.VideoView;
 
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
+        videoBG = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"
+                + getPackageName()
+                + "/"
+                + R.raw.sfondo);
 
+        videoBG.setVideoURI(uri);
+        videoBG.start();
 
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                mMediaPlayer.setLooping(true);
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        videoBG.start();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mMediaPlayer.release();
+        mMediaPlayer = null;
     }
 }
