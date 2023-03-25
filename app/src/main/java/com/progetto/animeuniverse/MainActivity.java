80f3ecb716f219f584e39f4328e038bec50fb79b
Dan1720 <d.gatti19@campus.unimib.it>
2023-03-25 17:05:39 +0100

    Modifica WelcomeActivity e navigazione tra i fragment

--- app/src/main/java/com/progetto/animeuniverse/MainActivity.java
+++ app/src/main/java/com/progetto/animeuniverse/MainActivity.java
@@ -4,12 +4,15 @@ package com.progetto.animeuniverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.Objects;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+
 
         videoBG = (VideoView) findViewById(R.id.videoView);
         Uri uri = Uri.parse("android.resource://"
@@ -41,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
                     mMediaPlayer.seekTo(mCurrentVideoPosition);
                     mMediaPlayer.start();
                 }
+            }
+        });
+
+        //QUESTO PEZZO E' SOLO PER LA FASE DI TEST
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
             }
         });
 
--- app/src/main/java/com/progetto/animeuniverse/WelcomeActivity.java
+++ app/src/main/java/com/progetto/animeuniverse/WelcomeActivity.java
@@ -1,8 +1,15 @@
 package com.progetto.animeuniverse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
@@ -12,6 +19,26 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+        Toolbar toolbar = findViewById(R.id.top_appbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.listFragment, R.id.notificationsFragment,
+                R.id.searchFragment, R.id.accountFragment
+        ).build();
+
+        // For the Toolbar
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // For the BottomNavigationView
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+
     }
 }
