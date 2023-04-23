0a6ffb977642e3aad9959d6403d6f1388f4d53e2
Dan1720 <d.gatti19@campus.unimib.it>
2023-04-23 16:56:55 +0200

    Aggiunto recyclerView e prova con arrayList

---
+++ app/src/main/java/com/progetto/animeuniverse/SearchListAdapter.java
@@ -0,0 +1,67 @@
+package com.progetto.animeuniverse;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class SearchListAdapter extends RecyclerView.Adapter<SearchListAdapter.AnimeViewHolder> {
+
+    public interface OnItemClickListener{
+        void onAnimeClick(Anime anime);
+    }
+    private List<Anime> animeList;
+    private OnItemClickListener onItemSelectedListener;
+    public SearchListAdapter(List<Anime> animeList, OnItemClickListener onItemSelectedListener){
+        this.animeList = animeList;
+        this.onItemSelectedListener = onItemSelectedListener;
+    }
+
+    @NonNull
+    @Override
+    public AnimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.search_anime_list_item,parent, false);
+        return new AnimeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnimeViewHolder holder, int position) {
+        holder.bind(animeList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(animeList != null){
+            return animeList.size();
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public class AnimeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private final TextView textViewTitle;
+        private final TextView textViewAuthor;
+        public AnimeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textview_title);
+            textViewAuthor = itemView.findViewById(R.id.textview_author);
+            itemView.setOnClickListener(this);
+        }
+        public void bind(Anime anime){
+            textViewTitle.setText(anime.getTitle());
+            textViewAuthor.setText(anime.getAuthor());
+        }
+
+        @Override
+        public void onClick(View v) {
+            onItemSelectedListener.onAnimeClick(animeList.get(getAdapterPosition()));
+        }
+    }
+}
