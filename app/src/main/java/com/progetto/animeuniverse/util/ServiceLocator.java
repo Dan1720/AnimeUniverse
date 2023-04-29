0890d30fc1abcaaec84c0b8f26702c082b542acd
Vivi1824 <v.giuliani3@campus.unimib.it>
2023-04-29 11:12:38 +0200

    Problemi gradle

--- app/src/main/java/com/progetto/animeuniverse/util/ServiceLocator.java
+++ app/src/main/java/com/progetto/animeuniverse/util/ServiceLocator.java
@@ -2,12 +2,16 @@ package com.progetto.animeuniverse.util;
 
 import android.app.Application;
 
+import com.progetto.animeuniverse.data.service.AnimeApiService;
 import com.progetto.animeuniverse.data.source.BaseUserAuthenticationRemoteDataSource;
 import com.progetto.animeuniverse.data.source.BaseUserDataRemoteDataSource;
 import com.progetto.animeuniverse.data.source.UserAuthenticationRemoteDataSource;
 import com.progetto.animeuniverse.data.source.UserDataRemoteDataSource;
 import com.progetto.animeuniverse.repository.user.IUserRepository;
 import com.progetto.animeuniverse.repository.user.UserRepository;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceLocator {
     private static volatile ServiceLocator INSTANCE = null;
@@ -37,5 +41,12 @@ public class ServiceLocator {
         DataEncryptionUtil dataEncryptionUtil = new DataEncryptionUtil(application);
 
         return new UserRepository(userAuthenticationRemoteDataSource, userDataRemoteDataSource);
+    }
+
+    public static AnimeApiService getAnimeApiService(){
+        //Creazione oggetto retrofit
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.ANIME_API_BASE_URL).
+                addConverterFactory(GsonConverterFactory.create()).build();
+        return retrofit.create(AnimeApiService.class); //associazione di retrofit alla classe
     }
 }
