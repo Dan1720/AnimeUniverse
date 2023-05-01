ef42d3e4e84455a7283583f8e71a3a82238ddde9
Dan1720 <d.gatti19@campus.unimib.it>
2023-05-01 22:14:53 +0200

    prima visualizzazione notifica nel fragment

---
+++ app/src/main/java/com/progetto/animeuniverse/model/Notification.java
@@ -0,0 +1,21 @@
+package com.progetto.animeuniverse.model;
+
+public class Notification {
+    private String title;
+    private String text;
+
+    public Notification(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+}
--- app/src/main/java/com/progetto/animeuniverse/ui/main/NotificationsFragment.java
+++ app/src/main/java/com/progetto/animeuniverse/ui/main/NotificationsFragment.java
@@ -5,7 +5,6 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -21,15 +20,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import android.service.notification.NotificationListenerService;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.progetto.animeuniverse.R;
+import com.progetto.animeuniverse.model.Notification;
 import com.progetto.animeuniverse.util.Constants;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 
 public class NotificationsFragment extends Fragment {
@@ -37,6 +43,10 @@ public class NotificationsFragment extends Fragment {
 
     private String textTitle = "Titolo prova";
     private String textContent = "Prima notifica";
+    private boolean notificationSent = false;
+    private View rootView;
+    private ArrayList<Notification> notifications = new ArrayList<>();
+    private int id = 0;
 
     public NotificationsFragment() {
         // Required empty public constructor
@@ -50,41 +60,68 @@ public class NotificationsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         createNotificationChannel();
+        resetNotificationFlag();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Intent intent = new Intent(getContext(), NotificationsFragment.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), Constants.CHANNEL_ID)
-                .setSmallIcon(R.drawable.logo)
-                .setContentTitle(textTitle)
-                .setContentText(textContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        boolean var = shouldSendNotification();
+        System.out.println(var);
+        if(var){
+            Intent intent = new Intent(getContext(), this.getClass());
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), Constants.CHANNEL_ID)
+                    .setSmallIcon(R.drawable.logo)
+                    .setContentTitle(textTitle)
+                    .setContentText(textContent)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            notificationManager.notify(getNotificationId(), builder.build());
+            Notification notification = new Notification(textTitle, textContent);
+            notifications.add(notification);
+            LinearLayout notificationContainer = rootView.findViewById(R.id.notification_container);
+            notificationContainer.removeAllViews();
+            for (Notification notificationItem: notifications){
+                View notificationView = LayoutInflater.from(getContext()).inflate(R.layout.notification_item, notificationContainer, false);
+                TextView titleTextView = notificationView.findViewById(R.id.notification_title);
+                titleTextView.setText(notificationItem.getTitle());
+                TextView textTextView = notificationView.findViewById(R.id.notification_text);
+                textTextView.setText(notificationItem.getText());
+                notificationContainer.addView(notificationView);
+            }
+
+
+
+
+
+
         }
-        notificationManager.notify(getNotificationId(), builder.build());
+
+
     }
+
 
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
@@ -108,16 +145,24 @@ public class NotificationsFragment extends Fragment {
     }
 
     private boolean shouldSendNotification() {
-        SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
-        boolean isNotNotificationSent = preferences.getBoolean("notification_sent", false);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        boolean isNotNotificationSent = preferences.getBoolean("notification_sent_flag", false);
         if (!isNotNotificationSent) {
             SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("notification_sent", true);
+            editor.putBoolean("notification_sent_flag", true);
             editor.apply();
             return true;
         } else {
             return false;
         }
+
     }
+    private void resetNotificationFlag() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("notification_sent_flag", false);
+        editor.apply();
+    }
+
 
 }
